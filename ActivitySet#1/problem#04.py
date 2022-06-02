c19fe096370ad0fb08373bc9a574a8cd9ea9e1ad
Shankar Emani <94682274+Shankaremani@users.noreply.github.com>
2022-06-02 09:53:05 +0000

    4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,16 @@
-# Conditional Execution
-
 hrs = input("Enter hours? ")
+xecution
+def Earning(hrs,rate):
+  wage = hrs*rate
+  return wage
+  
+def output(wage):
+	print("Earning",wage)
+  
+def main():
+	hrs=float(input("Enter hours:"))
+	rate=float(input("Enter rate:"))
+	wage=Earning(hrs,rate)
+	output(wage)
+  
+main()
